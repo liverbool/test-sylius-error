4b3141033241865ef2b9dcd9587db3c51da6fd2a
liverbool <nukboon@gmail.com>
2016-07-29 22:18:44 +0700

    Circular reference detected exception.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,13 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            new \Sylius\Bundle\ResourceBundle\SyliusResourceBundle(),
+            new \Sylius\Bundle\LocaleBundle\SyliusLocaleBundle(),
+
+            new \FOS\RestBundle\FOSRestBundle(),
+            new \JMS\SerializerBundle\JMSSerializerBundle(),
+            new \winzou\Bundle\StateMachineBundle\winzouStateMachineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
